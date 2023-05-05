f3bee80838b2f91618bd485a7aab950480cb8a9c
Max Filippov <jcmvbkbc@gmail.com>
2023-05-05 14:15:40 -0700

    WIP: esp32s3/.../ext_mem_defs.h: fix MMU entry allocation for IMEM
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- components/soc/esp32s3/include/soc/ext_mem_defs.h
+++ components/soc/esp32s3/include/soc/ext_mem_defs.h
@@ -124,7 +124,7 @@ _Static_assert(SOC_MMU_IRAM0_LINEAR_ADDRESS_LOW == SOC_MMU_DRAM0_LINEAR_ADDRESS_
 #define CACHE_DBUS_MMU_END              0x800
 
 #define CACHE_IROM_MMU_START            0
-#define CACHE_IROM_MMU_END              Cache_Get_IROM_MMU_End()
+#define CACHE_IROM_MMU_END              (0x1ff * 4)
 #define CACHE_IROM_MMU_SIZE             (CACHE_IROM_MMU_END - CACHE_IROM_MMU_START)
 
 #define CACHE_DROM_MMU_START            CACHE_IROM_MMU_END
